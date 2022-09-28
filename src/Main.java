c062c1ac13c4b33fef15d6fb8a8d42bf2673b1eb
SantiagoVira <santiagovira06@gmail.com>
2022-09-28 12:20:05 -0400

    Add more specificity

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Setup
+        Scanner input = new Scanner(System.in);
+
+        // Get values from user
+        System.out.print("How much is the bill? ");
+        double totalBill = input.nextDouble();
+        System.out.print("How much would you like to tip (in percentage, ex. 20)? ");
+        double tipPercentage = input.nextDouble();
+        System.out.print("How many people are there? ");
+        int numPeople = input.nextInt();
+
+        // Calculate total tip
+        double totalTip = totalBill * (tipPercentage / 100);
+
+        // Calculate values per person
+        double tipPerPerson = totalTip / numPeople;
+        double billAmountPerPerson = totalBill / numPeople;
+        double totalPerPerson = tipPerPerson + billAmountPerPerson;
+
+        // Format output variables
+        String tipPerPersonFormatted = String.format("%.2f",tipPerPerson);
+        String billPerPersonFormatted = String.format("%.2f",billAmountPerPerson);
+        String totalPerPersonFormatted = String.format("%.2f",totalPerPerson);
+
+        // Output values to user
+        System.out.println("Each person will pay $" + billPerPersonFormatted +
+                " for the bill and will tip $" +
+                tipPerPersonFormatted + "For a total of $" + totalPerPersonFormatted + " per person.");
     }
 }
